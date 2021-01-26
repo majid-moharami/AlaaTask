ec059f3f0fde8b88d91087f8f76c7f69fc2a4232
majid-moharami <majid.moharami79@gmail.com>
2021-01-26 18:44:08 +0330

    develop ListFragmentViewModel class, in this ViewModel there is a requestForData method that fetch data array from api and separate sets from data and set in liveData. in SetListFragment create viewModel instance and set observer for liveData.

--- app/src/main/java/com/example/alaatask/ui/fragment/SetListFragment.java
+++ app/src/main/java/com/example/alaatask/ui/fragment/SetListFragment.java
@@ -1,30 +1,22 @@
 package com.example.alaatask.ui.fragment;
 
 import android.os.Bundle;
-
-import androidx.databinding.DataBindingUtil;
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.alaatask.R;
 import com.example.alaatask.databinding.FragmentSetListBinding;
-import com.example.alaatask.data.model.Data;
-import com.example.alaatask.network.AlaaTvApi;
-import com.example.alaatask.network.RetrofitInstance;
+import com.example.alaatask.viewmodel.ListFragmentViewModel;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class SetListFragment extends Fragment {
     private FragmentSetListBinding mBinding;
-//    private Retrofit mRetrofit = RetrofitInstance.getRetrofitInstance();
-//    private AlaaTvApi mTvApi;
+    private ListFragmentViewModel mViewModel;
 
     public static SetListFragment newInstance() {
         SetListFragment fragment = new SetListFragment();
@@ -36,32 +28,22 @@ public class SetListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        mTvApi = mRetrofit.create(AlaaTvApi.class);
-//        Call<Data> Call;
-//
-//        Call = mTvApi.getAllData();
-//        Call.enqueue(new Callback<Data>() {
-//            @Override
-//            public void onResponse(retrofit2.Call<Data> call, Response<Data> response) {
-//                if (response.isSuccessful()){
-//                    Log.d("MAJID","YES");
-//                }
-//                Log.d("MAJID","YESs");
-//            }
-//
-//            @Override
-//            public void onFailure(retrofit2.Call<Data> call, Throwable t) {
-//                Log.d("MAJID","NO",t);
-//            }
-//        });
+        mViewModel = new ViewModelProvider(this).get(ListFragmentViewModel.class);
+        registerObserver();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_set_list, container, false );
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_set_list, container, false);
 
         return mBinding.getRoot();
+    }
+
+    private void registerObserver() {
+        mViewModel.getSetsMutableLiveData().observe(this, sets -> {
+
+        });
     }
 }
--- app/src/main/java/com/example/alaatask/viewmodel/ListFragmentViewModel.java
+++ app/src/main/java/com/example/alaatask/viewmodel/ListFragmentViewModel.java
@@ -1,4 +1,68 @@
 package com.example.alaatask.viewmodel;
 
-public class ListFragmentViewModel {
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.alaatask.data.model.Data;
+import com.example.alaatask.data.model.DataItem;
+import com.example.alaatask.data.model.Sets;
+import com.example.alaatask.network.AlaaTvApi;
+import com.example.alaatask.network.RetrofitInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class ListFragmentViewModel extends AndroidViewModel {
+    private Retrofit mRetrofit = RetrofitInstance.getRetrofitInstance();
+    private AlaaTvApi mTvApi;
+    private MutableLiveData<List<Sets>> mSetsMutableLiveData = new MutableLiveData<>();
+
+    @Inject
+    public ListFragmentViewModel(@NonNull Application application) {
+        super(application);
+        requestForData();
+    }
+
+    public void requestForData(){
+        mTvApi = mRetrofit.create(AlaaTvApi.class);
+        Call<Data> Call;
+
+        Call = mTvApi.getAllData();
+        Call.enqueue(new Callback<Data>() {
+            @Override
+            public void onResponse(retrofit2.Call<Data> call, Response<Data> response) {
+                if (response.isSuccessful()){
+                    Log.d("MAJID","Successful");
+                    List<Sets> setsList = new ArrayList<>();
+                    List<DataItem> dataItems = response.body().getData();
+                    for (int i = 0; i < dataItems.size(); i++) {
+                        if ( dataItems.get(i).getSets() != null || dataItems.get(i).getSets().size() != 0){
+                            setsList.addAll(dataItems.get(i).getSets());
+                        }
+                    }
+                    mSetsMutableLiveData.setValue(setsList);
+                }
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<Data> call, Throwable t) {
+                Log.d("MAJID","NotSuccessful",t);
+            }
+        });
+    }
+
+    public MutableLiveData<List<Sets>> getSetsMutableLiveData() {
+        return mSetsMutableLiveData;
+    }
 }
