6d7e8b4a0ceabe98338354131308701447713fdf
majid-moharami <majid.moharami79@gmail.com>
2021-01-26 13:47:50 +0330

    use DataBinding for inflate layout. use SetListFragment in MainActivity.

--- app/src/main/java/com/example/alaatask/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/alaatask/ui/activity/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.alaatask.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.alaatask.R;
+import com.example.alaatask.databinding.ActivityMainBinding;
+import com.example.alaatask.databinding.ActivityMainBindingImpl;
+import com.example.alaatask.databinding.FragmentSetListBindingImpl;
+import com.example.alaatask.ui.fragment.SetListFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(mBinding.fragmrntContainer.getId(), SetListFragment.newInstance())
+                .commit();
     }
 }
